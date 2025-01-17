3d46c01f30d565e2a048438ce459643750eb0ac8
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-01-17 03:03:22 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+const questions = [
+	{
+		question: "Which Nigerian city was the capital before it was moved to Abuja in 1991?",
+
+		answers: [
+			{option: "Lagos", correct: true},
+			{option: "Kano", correct: false},
+			{option: "Port Harcourt", correct: false},
+			{option: "Ibadan", correct: false}
+		]
+	},
+
+	{
+		question: "What natural wonder is located in Australia and is known for its stunning coral reefs?",
+
+		answers: [
+			{option: "The Grand Canyon", correct: false},
+			{option: "Mount Everest", correct: false},
+			{option: "The Great Barrier Reef", correct: true},
+			{option: "Niagara Falls", correct: false}
+		]
+	},
+
+	{
+		question: "Who was the first President of Nigeria after independence?",
+
+		answers: [
+			{option: "Nnamdi Azikiwe", correct: true},
+			{option: "Olusegun Obasanjo", correct: false},
+			{option: " Muhammadu Buhari", correct: false},
+			{option: "Tafawa Balewa", correct: false}
+		]
+	},
+
+	{
+		question:  "What is the largest desert in the world?",
+
+
+		answers: [
+			{option: "Sahara", correct: false},
+			{option: "Arctic", correct: true},
+			{option: "Kalahari", correct: false},
+			{option: "Gobi", correct: false}
+		]
+	},
+
+	{
+		question: "Which major civil conflict took place in Nigeria between 1967 and 1970?",
+
+		answers: [
+			{option: "The Nigerian Civil War", correct: false},
+			{option: "Yoruba War", correct: false},
+			{option: "Ogoni Crisis", correct: false},
+			{option: "Biafran War", correct: true}
+		]
+	},
+
+	{
+		question: "What is the capital city of Japan?",
+
+		answers: [
+			{option: "Seoul", correct: false},
+			{option: "Beijing", correct: false},
+			{option: "Tokyo", correct: true},
+			{option: "Bangkok", correct: false}
+		]
+	},
+
+	{
+		question: "Which country is known as the Land of the Rising Sun?",
+
+		answers: [
+			{option: "Japan", correct: true},
+			{option: "China", correct: false},
+			{option: "Thailand", correct: false},
+			{option: "India", correct: false}
+		]
+	},
+
+	{
+		question: "Which Nigerian city was the capital before it was moved to Abuja in 1991?",
+
+		answers: [
+			{option: "Lagos", correct: true},
+			{option: "Kano", correct: false},
+			{option: "Port Harcourt", correct: false},
+			{option: "Ibadan", correct: false}
+		]
+	},
+
+	{
+		question: "What year did Nigeria gain its independence from British colonial rule?",
+
+		answers: [
+			{option: "1956", correct: false},
+			{option: "1970", correct: false},
+			{option: "1963", correct: false},
+			{option: "1960", correct: true}
+		]
+	},
+
+	{
+		question: "What is the name of Nigeria's currency?",
+
+		answers: [
+			{option: "Naira", correct: true},
+			{option: "Yen", correct: false},
+			{option: "Dollar", correct: false},
+			{option: "Pound", correct: false}
+		]
+	},
+
+	{
+		question: "Which city is famous for its iconic Eiffel Tower?",
+
+		answers: [
+			{option: "Madrid", correct: false},
+			{option: "London", correct: false},
+			{option: "Paris", correct: true},
+			{option: "Rome", correct: false}
+		]
+	},
+]
