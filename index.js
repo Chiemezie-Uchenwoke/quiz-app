73b894ddf1db4b7872b3f40739a11ef4ed368d70
Chiemezie Uchenwoke <uchenwoke.chiemezie@gmail.com>
2025-01-19 20:15:56 +0100

    restructure quiz app and add an intro page

--- index.js
+++ index.js
@@ -1,114 +1,214 @@
 const questions = [
 	{
-		question: "Which Nigerian city was the capital before it was moved to Abuja in 1991?",
+		question: `What is the correct syntax for referring to an external script called "app.js" ?`,
 
 		answers: [
-			{option: "Lagos", correct: true},
-			{option: "Kano", correct: false},
-			{option: "Port Harcourt", correct: false},
-			{option: "Ibadan", correct: false}
+			{option: `<script href="app.js"></script>`, correct: false},
+			{option: `<script name="app.js"></script>`, correct: false},
+			{option: `<script src="app.js"></script>`, correct: true},
+			{option: `<script file="app.js"></script>`, correct: false}
 		]
 	},
 
 	{
-		question: "What natural wonder is located in Australia and is known for its stunning coral reefs?",
+		question: `How do you write "Hello, World!" in an alert box?`,
 
 		answers: [
-			{option: "The Grand Canyon", correct: false},
-			{option: "Mount Everest", correct: false},
-			{option: "The Great Barrier Reef", correct: true},
-			{option: "Niagara Falls", correct: false}
+			{option: `alertBox("Hello, World!");`, correct: false},
+			{option: `alert("Hello, World!");`, correct: true},
+			{option: `msgBox("Hello, World!");`, correct: false},
+			{option: `msg("Hello, World!");`, correct: false}
 		]
 	},
 
 	{
-		question: "Who was the first President of Nigeria after independence?",
+		question: "How do you create a function in JavaScript?",
 
 		answers: [
-			{option: "Nnamdi Azikiwe", correct: true},
-			{option: "Olusegun Obasanjo", correct: false},
-			{option: " Muhammadu Buhari", correct: false},
-			{option: "Tafawa Balewa", correct: false}
+			{option: "function = myFunction()", correct: false},
+			{option: "function myFunction()", correct: true},
+			{option: "function:myFunction()", correct: false},
+			{option: "create myFunction()", correct: false}
 		]
 	},
 
 	{
-		question:  "What is the largest desert in the world?",
+		question:  "How do you call a function named myFunction?",
 
 
 		answers: [
-			{option: "Sahara", correct: false},
-			{option: "Arctic", correct: true},
-			{option: "Kalahari", correct: false},
-			{option: "Gobi", correct: false}
+			{option: "call myFunction()", correct: false},
+			{option: "myFunction()", correct: true},
+			{option: "call function myFunction()", correct: false},
+			{option: "execute myFunction()", correct: false}
 		]
 	},
 
 	{
-		question: "Which major civil conflict took place in Nigeria between 1967 and 1970?",
+		question: "How can you add a single-line comment in JavaScript?",
 
 		answers: [
-			{option: "The Nigerian Civil War", correct: false},
-			{option: "Yoruba War", correct: false},
-			{option: "Ogoni Crisis", correct: false},
-			{option: "Biafran War", correct: true}
+			{option: "<!-- This is a comment -->", correct: false},
+			{option: "// This is a comment", correct: true},
+			{option: "/* This is a comment */", correct: false},
+			{option: "# This is a comment", correct: false}
 		]
 	},
 
 	{
-		question: "What is the capital city of Japan?",
+		question: "How do you declare a JavaScript variable?",
 
 		answers: [
-			{option: "Seoul", correct: false},
-			{option: "Beijing", correct: false},
-			{option: "Tokyo", correct: true},
-			{option: "Bangkok", correct: false}
+			{option: "variable x;", correct: false},
+			{option: "declare x;", correct: false},
+			{option: "x variable;", correct: false},
+			{option: "let x;", correct: true}
 		]
 	},
 
 	{
-		question: "Which country is known as the Land of the Rising Sun?",
+		question: "What is the correct way to check if x is equal to 5 in JavaScript?",
 
 		answers: [
-			{option: "Japan", correct: true},
-			{option: "China", correct: false},
-			{option: "Thailand", correct: false},
-			{option: "India", correct: false}
+			{option: "if (x = 5)", correct: false},
+			{option: "if (x == 5)", correct: false},
+			{option: "if x == 5 then", correct: false},
+			{option: "if (x === 5)", correct: true}
 		]
 	},
 
 	{
-		question: "What year did Nigeria gain its independence from British colonial rule?",
+		question: "What is the output of console.log(typeof 42);?",
 
 		answers: [
-			{option: "1956", correct: false},
-			{option: "1970", correct: false},
-			{option: "1963", correct: false},
-			{option: "1960", correct: true}
+			{option: "number", correct: true},
+			{option: "string", correct: false},
+			{option: "boolean", correct: false},
+			{option: "object", correct: false}
 		]
 	},
 
 	{
-		question: "What is the name of Nigeria's currency?",
+		question: "What does NaN stand for in JavaScript?",
 
 		answers: [
-			{option: "Naira", correct: true},
-			{option: "Yen", correct: false},
-			{option: "Dollar", correct: false},
-			{option: "Pound", correct: false}
+			{option: "Not a Null", correct: false},
+			{option: "No Assigned Name", correct: false},
+			{option: "Not a Number", correct: true},
+			{option: "None Applicable", correct: false}
 		]
 	},
 
 	{
-		question: "Which city is famous for its iconic Eiffel Tower?",
+		question: "Which event occurs when the user clicks on an HTML element?",
 
 		answers: [
-			{option: "Madrid", correct: false},
-			{option: "London", correct: false},
-			{option: "Paris", correct: true},
-			{option: "Rome", correct: false}
+			{option: "onmouseover", correct: false},
+			{option: "onclick", correct: true},
+			{option: "onchange", correct: false},
+			{option: "onmouseclick", correct: false}
 		]
 	},
+	{
+		question: "What is the result of true && false in JavaScript?",
+
+		answers: [
+			{option: "true", correct: false},
+			{option: "false", correct: true},
+			{option: "undefined", correct: false},
+			{option: "null", correct: false}
+		]
+	},
+	{
+		question: "How do you round a number to the nearest integer in JavaScript?",
+
+		answers: [
+			{option: "Math.round()", correct: true},
+			{option: "Math.floor()", correct: false},
+			{option: "Math.ceil()", correct: false},
+			{option: "Math.random()", correct: false}
+		]
+	},
+	{
+		question: "Which method is used to add an element to the end of an array?",
+
+		answers: [
+			{option: "pop()", correct: false},
+			{option: "shift()", correct: false},
+			{option: "unshift()", correct: false},
+			{option: "push()", correct: true}
+		]
+	},
+	{
+		question: "What is the correct way to declare a constant in JavaScript?",
+
+		answers: [
+			{option: "let constant x = 5;", correct: false},
+			{option: "const x = 5;", correct: true},
+			{option: "constant x = 5;", correct: false},
+			{option: "var x = 5;", correct: false}
+		]
+	},
+	{
+		question: `What will the code console.log(2 + "2"); output?`,
+
+		answers: [
+			{option: "4", correct: false},
+			{option: "NaN", correct: false},
+			{option: "22", correct: true},
+			{option: "undefined", correct: false}
+		]
+	},
+	{
+		question: `How do you remove the last element from an array?`,
+
+		answers: [
+			{option: "array.push()", correct: false},
+			{option: "array.shift()", correct: false},
+			{option: "array.splice()", correct: false},
+			{option: "array.pop()", correct: true}
+		]
+	},
+	{
+		question: `Which of the following methods can be used to combine two arrays in JavaScript?`,
+
+		answers: [
+			{option: "push()", correct: false},
+			{option: "join()", correct: false},
+			{option: "splice()", correct: false},
+			{option: "concat()", correct: true}
+		]
+	},
+	{
+		question: `What will the following code console.log(1 + "2" + 3); output?`,
+
+		answers: [
+			{option: "6", correct: false},
+			{option: "123", correct: true},
+			{option: "33", correct: false},
+			{option: "15", correct: false}
+		]
+	},
+	{
+		question: `What is the value of !!false`,
+
+		answers: [
+			{option: "true", correct: false},
+			{option: "false", correct: true},
+			{option: "null", correct: false},
+			{option: "undefined", correct: false}
+		]
+	},
+	{
+		question: `Which method converts a JSON object into a string?`,
+
+		answers: [
+			{option: "JSON.parse", correct: false},
+			{option: "toString", correct: false},
+			{option: "JSON.stringify", correct: true},
+			{option: "String", correct: false}
+		]
+	}
 ]
 
 /* 
@@ -182,7 +282,7 @@ const displayScore = () => {
 	document.querySelector(".quiz_question").style.alignItems = "center";
 
 	reloadButton.addEventListener("click", () => {
-		location.reload();
+		location.href = "./index.html";
 	});
 }
 
